748dcbf5eb761fa19baeb23f4caefff8dc400330
bmconquest <bmconquest@crimson.ua.edu>
2019-10-27 17:48:21 -0500

    added src and include directories for clean code

--- package.c
+++ package.c
@@ -5,7 +5,6 @@
 
 #ifdef DEBUG
 #endif
-#define MAX_MATRIX_DIMENSION 50
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,19 +16,5 @@ int main(int argc, char *argv[])
 		printf("Debug mode is on.\n");
 	}
 
-	int matrix[3][4];
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 4; j++) {
-			matrix[i][j] = i+j;
-		}
-	}
-
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 4; j++) {
-			printf("%d ", matrix[i][j]);
-		}
-		printf("\n");
-	}
-
-	return 1;
+	return 0;
 }
