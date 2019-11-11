85e7d6e206240d7d137dbbd2e629d03bef7a69ca
Bryant M. Conquest <bmconquest@cs426.ua.edu>
2019-11-11 14:28:48 -0600

    added signal handler

--- package.c
+++ package.c
@@ -6,44 +6,45 @@
 #ifdef DEBUG
 #endif
 
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "./include/error.h"
 #include "./include/matrix.h"
 
+volatile int sent;
+volatile int recv;
+
+void sig_handler(int signo) {
+	if (signo == SIGINT)
+		printf("Jobs Sent %d Jobs Received %d\n", sent, recv);
+}
+
 int main(int argc, char *argv[])
 {
+	signal(SIGINT, sig_handler);
+
 	if(DEBUG) {
 		printf("Debug mode is on.\n");
 	}
-  
-  if (argc != 5) {
-    printf("Usage is ./package <matrix 1 filename> <matrix 2 filename>"
-           " <output matrix data file> <secs between thread creation>\n");
-    Ferror("Check Arguments");
-  }
 
-  matrix *matrix1 = initMatrix(argv[1]);
-  matrix *matrix2 = initMatrix(argv[2]);
-  
-  checkMatrix(matrix1, matrix2);
+	if (argc != 5) {
+		printf("Usage is ./package <matrix 1 filename> <matrix 2 filename>"
+				" <output matrix data file> <secs between thread creation>\n");
+		Ferror("Check Arguments");
+	}
 
-  /*int *temp = getRow(matrix1, 0);
-  int *temp2 = getCol(matrix1, 0);
+	matrix *matrix1 = initMatrix(argv[1]);
+	matrix *matrix2 = initMatrix(argv[2]);
 
-  for(int i = 0; i < matrix1->r; i++) {
-    printf("%d\n", temp2[i]);
-  }
-  for(int i = 0; i < matrix1->c; i++) {
-    printf("%d ", temp[i]);
-  }
-  printf("\n");
-  
-  free(temp);
-  free(temp2);*/
-  destroyMatrix(matrix1);
-  destroyMatrix(matrix2);
-  return 0;
+	checkMatrix(matrix1, matrix2);
+	
+	destroyMatrix(matrix1);
+	destroyMatrix(matrix2);
+	while(1) sleep(1);
+	return 0;
 }
