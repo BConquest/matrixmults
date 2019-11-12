1665d8b35e69b4ed2e96bcf8b0467e147836413b
bmconquest <bmconquest@crimson.ua.edu>
2019-11-11 19:52:10 -0600

    Updated package

--- compute.c
+++ compute.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+struct mesg_buffer {
+  long msg_type;
+  char msg_text[100];
+} message;
 
 int main(void) {
+  key_t key;
+  int msgid;
+  int id = (int)"bmconquest";
+
+  key = ftok("./bmconquest", id);
+  printf("key -> %d\n",key);
+  msgid = msgget(key, 0666 | IPC_CREAT);
+
+  msgrcv(msgid, &message, sizeof(message), 1, 0);
+  printf("Data Recieved is : %s\n",
+          message.msg_text);
+
+  msgctl(msgid, IPC_RMID, NULL);
+  return 0;
 }
--- package.c
+++ package.c
@@ -12,6 +12,8 @@
 #include <pthread.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 
 #include "./include/error.h"
 #include "./include/matrix.h"
@@ -23,6 +25,11 @@ void sig_handler(int signo) {
 	if (signo == SIGINT)
 		printf("Jobs Sent %d Jobs Received %d\n", sent, recv);
 }
+
+struct mesg_buffer {
+  long mesg_type;
+  char mesg_text[100];
+} message;
 
 int main(int argc, char *argv[])
 {
@@ -42,12 +49,27 @@ int main(int argc, char *argv[])
 	matrix *matrix2 = initMatrix(argv[2]);
 
 	checkMatrix(matrix1, matrix2);
-	
+  printf("1\n");
+  key_t key;
+  int msgid;
+  int id = (int)"bmconquest";
+
+  key = ftok("./bmconquest", id);
+  printf("2\n");
+  msgid = msgget(key, 0666 | IPC_CREAT);
+  printf("3\n");
+  message.mesg_type = 1;
+
+  printf("Write Data : ");
+  gets(message.mesg_text);
+  printf("4\n");
+  msgsnd(msgid, &message, sizeof(message), 0);
+
+  if(DEBUG) {
+    printf("key->%d\n",key);
+  }
+
 	destroyMatrix(matrix1);
 	destroyMatrix(matrix2);
-	while(1) {
-    sent++;
-    recv--;
-  }
 	return 0;
 }
