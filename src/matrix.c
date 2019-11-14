9400fc4a97f2f1c5449a7e603ce43469ed630a87
BConquest <bmconquest@crimson.ua.edu>
2019-11-14 11:46:29 -0600

    Updated matrix.c to have more consistent code style

--- src/matrix.c
+++ src/matrix.c
@@ -3,29 +3,21 @@
 int *allocateMatrix(int rows, int cols)
 {
 	int *ptr;
-	int i;
 
 	ptr = calloc((rows*cols),sizeof(int));
-
-	if (!ptr) {
-		Ferror("Cannot allocate space for matrix");
-	}
+	if (!ptr) Ferror("Cannot allocate space for matrix");
 	
 	return ptr;
 }
 
 int *getCol(matrix *a, int col)
 {
-	if (a->c <= col) {
-		Ferror("Invalid col");
-	}
+	if (a->c <= col) Ferror("Invalid col");
 
 	int r = 0;
 	int *newCol = calloc(a->r,sizeof(int));
 	
-	if (!newCol) {
-		Ferror("Could not allocate space for new col");
-	}
+	if (!newCol) Ferror("Could not allocate space for new col");
 	
 	printf("%d %d\t", col, a->r);
 
@@ -33,25 +25,21 @@ int *getCol(matrix *a, int col)
 		printf("%d ", col+((a->r+1)*r));
 		newCol[r] = a->pmatrix[col+((a->r+1)*r)];
 	}
+
 	printf("\n");
 	return newCol;
 }
 
 int *getRow(matrix *a, int row)
 {
-	if (a->r <= row) {
-		Ferror("Invalid row");
-	}
+	if (a->r <= row) Ferror("Invalid row");
+	
 	int c = 0;
 	int* newRow = calloc(a->c,sizeof(int));
+	
+	if (!newRow) Ferror("Could not allocate space for new row");
+	for (c = 0; c < a->c; c++) newRow[c] = a->pmatrix[(row*a->c)+c];
 
-	if (!newRow) {
-		Ferror("Could not allocate space for new row");
-	}
-
-	for (c = 0; c < a->c; c++) {
-		newRow[c] = a->pmatrix[(row*a->c)+c];
-	}
 	return newRow;
 }
 
@@ -70,39 +58,33 @@ matrix *initMatrix(char *filename)
 
 	fscanf(fileptr, "%d", &temp_rows);
 	fscanf(fileptr, "%d", &temp_cols);
-	if (DEBUG)
-		printf("matrix dimensions r: %d c: %d\n", temp_rows, temp_cols);
+	if (DEBUG) printf("matrix dimensions r: %d c: %d\n"
+			, temp_rows, temp_cols);
 
 	temp_matrix = allocateMatrix(temp_rows, temp_cols);
 
 	for (i = 0; i < temp_rows*temp_cols; i++)
 		fscanf(fileptr, "%d", &temp_matrix[i]);
 	if (i != temp_rows*temp_cols && DEBUG)
-		printf("Input matrix did not contain enough values initiliazed to 0\n");
+		printf("Input matrix did not contain enough values initiliazed"
+		       "to 0\n");
 
-	if (DEBUG)
-		printMatrix(temp_matrix, temp_rows, temp_cols);
+	if (DEBUG) printMatrix(temp_matrix, temp_rows, temp_cols);
 
-	matrix *matrix1 = malloc(sizeof(matrix));
-	matrix1->r = temp_rows;
-	matrix1->c = temp_cols;
-	matrix1->pmatrix = temp_matrix;
+	matrix *a = malloc(sizeof(matrix));
+	a->r = temp_rows;
+	a->c = temp_cols;
+	a->pmatrix = temp_matrix;
 
 	fclose(fileptr);
-	return matrix1;
+	return a;
 }
 
 void checkMatrix(matrix *a, matrix *b)
 {
-	if (a->c != b->r) {
-		Ferror("Matricies do not correlate");
-	}
-	if (a->r > 50 || a->c > 50) {
-		Ferror("Matrix a is too large");
-	}
-	if (b->r > 50 || b->c > 50) {
-		Ferror("Matrix b is too large");
-	}
+	if (a->c != b->r) Ferror("Matricies do not correlate");
+	if (a->r > 50 || a->c > 50) Ferror("Matrix a is too large");
+	if (b->r > 50 || b->c > 50) Ferror("Matrix b is too large");
 }
 
 void destroyMatrix(matrix *del)
@@ -113,13 +95,10 @@ void destroyMatrix(matrix *del)
 
 void printMatrix(int *ptr, int rows, int cols)
 {
-	int i, j, c = 0;
+	int i = 0, j = 0, c = 0;
 
 	for (i = 0; i < rows; i++) {
-		for (j = 0; j < cols; j++) {
-			printf("%d ", ptr[c]);
-			c++;
-		}
+		for (j = 0; j < cols; j++) printf("%d ", ptr[c++]);
 		printf("\n");
 	}
 }
